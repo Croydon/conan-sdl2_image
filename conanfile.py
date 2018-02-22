fd9f9544bdc4793521114cc3004c7dd9986c3e59
Croydon <cr0ydon@outlook.com>
2018-02-22 21:58:37 +0100

    Change libwebp back to stable channel

--- conanfile.py
+++ conanfile.py
@@ -17,7 +17,7 @@ class SDLConan(ConanFile):
     generators = "cmake"
     exports = ["CMakeLists.txt"]
     url = "https://github.com/bincrafters/conan-sdl2_image"
-    requires = "sdl2/2.0.7@bincrafters/stable", "libpng/1.6.34@bincrafters/stable", "libjpeg-turbo/1.5.2@bincrafters/stable", "libwebp/0.6.1@bincrafters/testing"
+    requires = "sdl2/2.0.7@bincrafters/stable", "libpng/1.6.34@bincrafters/stable", "libjpeg-turbo/1.5.2@bincrafters/stable", "libwebp/0.6.1@bincrafters/stable"
     license = "MIT"
     filename = "SDL2_image-%s" % version
 
